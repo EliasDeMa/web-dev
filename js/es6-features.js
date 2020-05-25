2cfcd03e5faf79416ce314438caf05ef28a97773
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-05-25 10:13:13 +0200

    test es6 features.

---
+++ js/es6-features.js
@@ -0,0 +1,25 @@
+class Pizza {
+  constructor(name = "pizza", toppings = []) {
+    this.name = name;
+    this.toppings = toppings;
+  }
+    
+  toString() {
+    return this.name;
+  }
+}
+  
+class HawaiiPizza extends Pizza {
+  constructor() {
+    super("pizza hawaii", ["tomato sauce", 'pineapple', 'mozarella']);
+  }
+}
+
+const arrow = () => {
+    console.log(this);
+}
+
+let pizza = new HawaiiPizza();
+  
+console.log(pizza.toString());
+console.log(pizza.toppings);
