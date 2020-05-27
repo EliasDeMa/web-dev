8f8b5839ac76d9f98a4b34049a99237424e5ea63
Elias De Maertelaere <elias.demaertelaere@gmail.com>
2020-05-27 10:59:19 +0200

    start of drawing on canvas.

---
+++ hangman/scripts/drawing.js
@@ -0,0 +1,9 @@
+export const drawGallow = (ctx) => {
+    ctx.moveTo(40, 380);
+    ctx.lineTo(160, 380);
+    ctx.moveTo(60, 380);
+    ctx.lineTo(60, 120);
+    ctx.lineTo(120, 120)
+    ctx.lineTo(120, 150)
+    ctx.stroke();
+}
--- hangman/scripts/index.js
+++ hangman/scripts/index.js
@@ -1,6 +1,8 @@
 import { HangMan, States } from './hangman.js';
+import { drawGallow } from './drawing.js';
 
 const url = 'https://random-word-api.herokuapp.com/word?number=1';
+const draws = [];
 
 const getWord = async (url) => {
     let result = await fetch(url);
@@ -21,11 +23,16 @@ let logWord;
 const submitButton = document.getElementById('submit-btn');
 const letter = document.getElementById('input-letter');
 const par = document.getElementById('letters');
+const myCanvas = document.getElementById('hangman-canvas');
+const ctx = myCanvas.getContext('2d');
 
-
+drawGallow(ctx);
 
 submitButton.addEventListener('click', () => {
     // body
     logWord.guess(letter.value);
     par.innerHTML = logWord.toString();
+    for (let i = 0; i < logWord.mistakes; i++) {
+        draws[i](ctx);
+    }
 });
